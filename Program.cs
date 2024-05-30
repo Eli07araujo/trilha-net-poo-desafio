40c5c905c9fe9f4b1f79884a19686877adad2e4a
Eli07araujo <eliseumotta07@gmail.com>
2024-05-30 14:34:45 -0300

    Implementação concluida, Programa funcionando

--- Program.cs
+++ Program.cs
@@ -1,3 +1,17 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+// Criação de um objeto Smartphone da classe Nokia
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "N91000", imei: "111111111", memoria: 256);
+nokia.Ligar();
+nokia.InstalarAplicativo("Mercado Livre");
+
+Console.WriteLine("\n");
+
+// Criação de um objeto Smartphone da classe Iphone
+Console.WriteLine("Smartphone Iphone:");
+Smartphone iphone = new Iphone(numero: "74108520", modelo: "15", imei: "555555555", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
